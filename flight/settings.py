0d97f84d1c39fd9fae0bba1aa9fefdc78a5cf001
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-09 18:07:57 +0500

    Deploy to Heroku

--- flight/settings.py
+++ flight/settings.py
@@ -109,12 +109,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
