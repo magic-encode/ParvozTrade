e2266a516f837fbbd71c100ddf67119ce657c084
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-09 17:05:23 +0500

    Deploy to Heroku

--- flight/settings.py
+++ flight/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-690d%5q2t9_&+gqh2lp_zapi-t!8uszzl+^uzv7faw4d7fl_i^'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
