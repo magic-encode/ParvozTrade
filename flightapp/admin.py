7c4f910ba548d851e18ffcc67da4dd0eaa184f9b
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-09 07:33:10 +0500

    Fixed best_seller

--- flightapp/admin.py
+++ flightapp/admin.py
@@ -3,27 +3,16 @@ from django.contrib import admin
 from flightapp.models.cart import Cart
 from flightapp.models.wishlist import Wishlist
 
-from flightapp.models.products import Brand 
-from flightapp.models.products import Banner
 from flightapp.models.products import Products
 from flightapp.models.products import Comments
-from flightapp.models.products import FeatureLeft
-from flightapp.models.products import BannerLefts
-from flightapp.models.products import FeatureRights
 
 from flightapp.models.order_history import OrderHistory
 
 from flightapp.models.category import Categories
 
 admin.site.register(Cart)
-admin.site.register(Brand)
-admin.site.register(Banner)
 admin.site.register(Products)
 admin.site.register(Comments)
 admin.site.register(Wishlist)
 admin.site.register(Categories)
-admin.site.register(BannerLefts)
-admin.site.register(FeatureLeft)
 admin.site.register(OrderHistory)
-admin.site.register(FeatureRights)
-
