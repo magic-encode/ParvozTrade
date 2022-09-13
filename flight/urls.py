0ae96160ad477feb3f8b56eb6694851387fffca8
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-13 10:54:27 +0500

    ParvozTrade cool project

--- flight/urls.py
+++ flight/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
-
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
 
+from django.urls import path
+from django.urls import re_path
+from django.urls import include
+
+from django.views.static import serve
+
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     
@@ -26,4 +30,7 @@ urlpatterns = [
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
          name="password_reset_complete"),
    
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
