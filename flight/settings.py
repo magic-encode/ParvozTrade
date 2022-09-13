f309bba3f473512d9a3c53bd16302d28e433b7b6
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-14 00:16:43 +0500

    Fixed Tests.py

--- flight/settings.py
+++ flight/settings.py
@@ -123,8 +123,8 @@ DEFAULT_FROM_EMAIL = 'Abdullajon Uzdeveloper'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env.str('EMAIL_HOST')
-EMAIL_PORT = env.int("EMAIL_PORT")
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_PORT = env.str("EMAIL_PORT")
+EMAIL_USE_TLS = env.str('EMAIL_USE_TLS')
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 
