5b539491ada53f1615c3d9395c68240c50b3ed56
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-24 07:28:46 +0500

    Fixed gmail setttings

--- flight/settings.py
+++ flight/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-DEFAULT_FROM_EMAIL = 'Abdullajon Uzdeveloper'
+DEFAULT_FROM_EMAIL = 'ParvozTrade'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env.str('EMAIL_HOST')
