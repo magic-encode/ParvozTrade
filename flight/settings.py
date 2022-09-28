1291f41825f03a1802a4f71fdeb5129cd184d428
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-09-28 16:53:55 +0500

    Fixed another files

--- flight/settings.py
+++ flight/settings.py
@@ -20,6 +20,9 @@ ALLOWED_HOSTS = ['127.0.0.1', 'parvoz-trade.uz',
 
 SITE_ID = 1
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
